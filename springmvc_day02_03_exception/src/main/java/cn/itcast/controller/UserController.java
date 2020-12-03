bc05fc64f697cc35759f255f9ec05ffb57b19190
cococolabear <794055465@qq.com>
2020-12-03 20:45:46 +0800

    springmvc异常处理

---
+++ springmvc_day02_03_exception/src/main/java/cn/itcast/controller/UserController.java
@@ -0,0 +1,15 @@
+package cn.itcast;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2020/12/3 5:31 下午
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+}
---
+++ springmvc_day02_03_exception/src/main/java/cn/itcast/exception/SysException.java
@@ -0,0 +1,6 @@
+package cn.itcast.exception;/**
+ * @author  Duan Xiangqing
+ * @date  2020/12/3 5:40 下午
+ * @version 1.0
+ */public class SysException {
+}
